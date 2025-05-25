00fde24b038f3163d9819c0a6a6cbe0a3749b9ad
Ava Gaiety W <ava@wroten.me>
2025-05-25 13:41:29 -0600

    remove magic strings, acronym function

--- lib/tournament.ex
+++ lib/tournament.ex
@@ -1,4 +1,13 @@
 defmodule Tournament do
+  @separator_input ";"
+  @separator_output " | "
+  @team "Team"
+  @matches "matches played"
+  @win "win"
+  @loss "loss"
+  @draw "draw"
+  @points "points"
+
   @doc """
   Given `input` lines representing two teams and whether the first of them won,
   lost, or reached a draw, separated by semicolons, calculate the statistics
@@ -12,7 +21,7 @@ defmodule Tournament do
   """
   @spec tally(input :: list(String.t())) :: String.t()
   def tally(input) do
-    initial_output = "Team                           | MP |  W |  D |  L |  P"
+    initial_output = "#{@team}                           | #{acronym(@matches)} |  #{acronym(@win)} |  #{acronym(@draw)} |  #{acronym(@loss)} |  #{acronym(@points)}"
 
     input
     |> Enum.reduce(%{}, fn line, state ->
@@ -21,20 +30,20 @@ defmodule Tournament do
       team2_tally = Map.get(state, team2, %{mp: 0, wins: 0, losses: 0, draws: 0, points: 0})
 
       case outcome do
-        "win" ->
+        @win ->
           state
-          |> update_tally(team1, update_team_map(team1_tally, "win"))
-          |> update_tally(team2, update_team_map(team2_tally, "loss"))
+          |> update_tally(team1, update_team_map(team1_tally, @win))
+          |> update_tally(team2, update_team_map(team2_tally, @loss))
 
-        "loss" ->
+        @loss ->
           state
-          |> update_tally(team1, update_team_map(team1_tally, "loss"))
-          |> update_tally(team2, update_team_map(team2_tally, "win"))
+          |> update_tally(team1, update_team_map(team1_tally, @loss))
+          |> update_tally(team2, update_team_map(team2_tally, @win))
 
-        "draw" ->
+        @draw ->
           state
-          |> update_tally(team1, update_team_map(team1_tally, "draw"))
-          |> update_tally(team2, update_team_map(team2_tally, "draw"))
+          |> update_tally(team1, update_team_map(team1_tally, @draw))
+          |> update_tally(team2, update_team_map(team2_tally, @draw))
       end
     end)
     |> Enum.to_list()
@@ -44,7 +53,7 @@ defmodule Tournament do
     end)
   end
 
-  defp parse_input(input), do: String.split(input, ";")
+  defp parse_input(input), do: String.split(input, @separator_input)
 
   defp team_stats_to_string(team, %{mp: mp, wins: wins, draws: draws, losses: losses, points: points}) do
     [
@@ -55,22 +64,29 @@ defmodule Tournament do
       String.pad_leading("#{losses}", 2),
       String.pad_leading("#{points}", 2)
     ]
-    |> Enum.join(" | ")
+    |> Enum.join(@separator_output)
   end
 
   defp update_tally(state, team, tally) do
     Map.put(state, team, tally)
   end
 
-  defp update_team_map(%{wins: wins, points: points, mp: mp} = team, "win") do
+  defp update_team_map(%{wins: wins, points: points, mp: mp} = team, @win) do
     %{team | wins: wins + 1, points: points + 3, mp: mp + 1}
   end
 
-  defp update_team_map(%{losses: losses, points: points, mp: mp} = team, "loss") do
+  defp update_team_map(%{losses: losses, points: points, mp: mp} = team, @loss) do
     %{team | losses: losses + 1, points: points + 0, mp: mp + 1}
   end
 
-  defp update_team_map(%{draws: draws, points: points, mp: mp} = team, "draw") do
+  defp update_team_map(%{draws: draws, points: points, mp: mp} = team, @draw) do
     %{team | draws: draws + 1, points: points + 1, mp: mp + 1}
   end
+
+  defp acronym(string), do:
+    string
+    |> String.split(" ")
+    |> Enum.map(&(String.at(&1, 0)))
+    |> Enum.join("")
+    |> String.upcase()
 end
